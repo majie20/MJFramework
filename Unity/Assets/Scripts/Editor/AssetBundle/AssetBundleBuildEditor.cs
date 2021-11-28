a01f5ff7bcd9c4e9d796cf7118407ceae2035462
majie <majie7171@qq.com>
2021-11-29 00:32:50 +0800

    添加AssetsBundleSettings

--- Unity/Assets/Scripts/Editor/AssetBundle/AssetBundleBuildEditor.cs
+++ Unity/Assets/Scripts/Editor/AssetBundle/AssetBundleBuildEditor.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -25,28 +27,71 @@ public class AssetBundleBuildEditor : EditorWindow
         }
 
         AssetDatabase.Refresh();
-        BuildPipeline.BuildAssetBundles(AB_PATH, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+
+        AssetsBundleSettings settings = AssetDatabase.LoadAssetAtPath<AssetsBundleSettings>("Assets/Resources/AssetsBundleSettings.asset");
+        List<AssetBundleBuild> buildList = new List<AssetBundleBuild>();
+
+        for (int i = 0; i < settings.fileDirABList.Count; i++)
+        {
+            var config = settings.fileDirABList[i];
+            var pathList = new List<string>();
+            Add(pathList, AssetDatabase.GetAssetPath(config.Dir), config);
+
+            AssetBundleBuild build = new AssetBundleBuild();
+            build.assetBundleName = config.ABName;
+            build.assetNames = pathList.ToArray();
+            buildList.Add(build);
+        }
+
+        BuildPipeline.BuildAssetBundles(AB_PATH, buildList.ToArray(), BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
         AssetDatabase.Refresh();
         Debug.Log($"完成AssetBundle打包，文件夹{AB_PATH}");
+    }
+
+    private static void Add(List<string> pathList, string path, FileDirABName config)
+    {
+        DirectoryInfo dir = new DirectoryInfo(path);
+        FileSystemInfo[] fileInfo = dir.GetFileSystemInfos();  //返回目录中所有文件和子目录
+        foreach (FileSystemInfo info in fileInfo)
+        {
+            //判断是否文件夹
+            if (info is DirectoryInfo)
+            {
+                Add(pathList, info.FullName, config);
+            }
+            else
+            {
+                if (info.Extension != ".meta")
+                {
+                    if (string.IsNullOrEmpty(config.Extension) || config.Extension.Contains(info.Extension))
+                    {
+                        string mp = info.FullName;
+                        mp = mp.Substring(mp.IndexOf("Assets", StringComparison.Ordinal));
+                        mp = mp.Replace('\\', '/');
+                        pathList.Add(mp);
+                    }
+                }
+            }
+        }
     }
 
     [MenuItem("Tools/AssetBundle/Zip AssetBundle")]
     public static void ZipAssetBundle()
     {
-        if (Directory.Exists(AB_ZIP_PATH))
-        {
-            Model.FileHelper.DelectDir(AB_ZIP_PATH);
-        }
-        else
-        {
-            Directory.CreateDirectory(AB_ZIP_PATH);
-        }
+        //if (Directory.Exists(AB_ZIP_PATH))
+        //{
+        //    Model.FileHelper.DelectDir(AB_ZIP_PATH);
+        //}
+        //else
+        //{
+        //    Directory.CreateDirectory(AB_ZIP_PATH);
+        //}
 
-        ZipWrapper.Zip(new[] { AB_PATH }, $"{AB_ZIP_PATH}/{ZIP_NAME}.ma", ZIP_PASSWORD);
+        //ZipWrapper.Zip(new[] { AB_PATH }, $"{AB_ZIP_PATH}/{ZIP_NAME}.ma", ZIP_PASSWORD);
 
-        Debug.Log($"完成AssetBundle包的压缩，文件{AB_ZIP_PATH}/{ZIP_NAME}.ma");
+        //Debug.Log($"完成AssetBundle包的压缩，文件{AB_ZIP_PATH}/{ZIP_NAME}.ma");
 
-        EditorUtility.RevealInFinder($"{AB_ZIP_PATH}");
+        //EditorUtility.RevealInFinder($"{AB_ZIP_PATH}");
     }
 
     [MenuItem("Tools/AssetBundle/Build And Zip AssetBundle")]
---
+++ Unity/Assets/Scripts/Editor/AssetsBundleSettingsEditor.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+//自定义ReferenceCollector类在界面中的显示与功能
+[CustomEditor(typeof(AssetsBundleSettings))]
+//没有该属性的编辑器在选中多个物体时会提示“Multi-object editing not supported”
+[CanEditMultipleObjects]
+public class AssetsBundleSettingsEditor : Editor
+{
+    private AssetsBundleSettings m_target;
+
+    /// <summary>
+    /// 滑动条位置
+    /// </summary>
+    private Vector2 m_ScrollPosition;
+
+    private void OnEnable()
+    {
+        this.m_target = (AssetsBundleSettings)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        var fileDirABList = m_target.fileDirABList;
+
+        if (GUILayout.Button("添加"))
+        {
+            fileDirABList.Add(new FileDirABName());
+        }
+
+        GUILayout.Space(10);
+
+        m_ScrollPosition = EditorGUILayout.BeginScrollView(m_ScrollPosition);
+
+        for (int i = 0; i < fileDirABList.Count; i++)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("ABName:", new GUIStyle { alignment = TextAnchor.LowerLeft, fixedWidth = 100 });
+            fileDirABList[i].ABName = EditorGUILayout.TextField(fileDirABList[i].ABName);
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("扩展名:", new GUIStyle { alignment = TextAnchor.LowerLeft, fixedWidth = 100 });
+            fileDirABList[i].Extension = EditorGUILayout.TextField(fileDirABList[i].Extension);
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("文件夹:", new GUIStyle { alignment = TextAnchor.LowerLeft, fixedWidth = 100 });
+            Object dir = EditorGUILayout.ObjectField(fileDirABList[i].Dir, typeof(Object), false);
+
+            if (Directory.Exists(AssetDatabase.GetAssetPath(dir)) || dir == null)
+            {
+                fileDirABList[i].Dir = dir;
+            }
+
+            if (GUILayout.Button("X"))
+            {
+                //将元素添加进删除list
+                fileDirABList.RemoveAt(i);
+            }
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(10);
+        }
+
+        EditorGUILayout.EndScrollView();  //结束 ScrollView 窗口
+
+        EditorUtility.SetDirty(target);
+    }
+}
